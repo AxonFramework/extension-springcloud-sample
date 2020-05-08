8a151e3183c7c2cd06a5acc505e5e67eb23ebe3e
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-05-08 10:44:24 +0200

    Fine tune sample project
    - Allow building with Java 11 though jaxb dependencies
    - Add .gitignore file
    - Default node.number property to zero if not present
    - Add custom SpringCloudCommandRouter bean creation to enforce http
    discovery

--- eureka-client/src/main/kotlin/org/axonframework/extension/springcloud/example/eureka/client/EurekaClientApplication.kt
+++ eureka-client/src/main/kotlin/org/axonframework/extension/springcloud/example/eureka/client/EurekaClientApplication.kt
@@ -1,8 +1,18 @@
 package org.axonframework.extension.springcloud.example.eureka.client
 
+import org.axonframework.commandhandling.distributed.RoutingStrategy
+import org.axonframework.extensions.springcloud.DistributedCommandBusProperties
+import org.axonframework.extensions.springcloud.commandhandling.SpringCloudCommandRouter
+import org.axonframework.extensions.springcloud.commandhandling.SpringCloudHttpBackupCommandRouter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.DiscoveryClient
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.serviceregistry.Registration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.web.client.RestTemplate
 
 /**
  * Simple Spring Boot application which is enabled as Eureka client through the [EnableDiscoveryClient] annotation.
@@ -13,5 +23,38 @@ fun main(args: Array<String>) {
 }
 
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = ["org.axonframework.extension.springcloud.example.giftcard"])
+@SpringBootApplication(scanBasePackages = [
+    "org.axonframework.extension.springcloud.example.giftcard",
+    "org.axonframework.extension.springcloud.example.eureka.client"
+])
 class EurekaClientApplication
+
+/**
+ * Eureka requires a custom [SpringCloudHttpBackupCommandRouter] configuration as we need to enforce the use of HTTP
+ * command discovery through the [SpringCloudHttpBackupCommandRouter.Builder.enforceHttpDiscovery] method.
+ */
+@Configuration
+class CommandRouterConfiguration {
+
+    @Bean
+    @Primary
+    fun springCloudCommandRouter(
+            discoveryClient: DiscoveryClient,
+            localServiceInstance: Registration,
+            restTemplate: RestTemplate,
+            routingStrategy: RoutingStrategy,
+            distributedCommandBusProperties: DistributedCommandBusProperties
+    ): SpringCloudCommandRouter {
+        return SpringCloudHttpBackupCommandRouter.builder()
+                .discoveryClient(discoveryClient)
+                .localServiceInstance(localServiceInstance)
+                .restTemplate(restTemplate)
+                .routingStrategy(routingStrategy)
+                .messageRoutingInformationEndpoint(distributedCommandBusProperties.springCloud.fallbackUrl)
+                .contextRootMetadataPropertyName(
+                        distributedCommandBusProperties.springCloud.contextRootMetadataPropertyName
+                )
+                .enforceHttpDiscovery()
+                .build()
+    }
+}
